5214dcebc21e7fe3bf188e6ca4915606bb21a480
Pranay Kumar Paine <pranaykumarpaine@gmail.com>
2023-03-06 17:25:18 +0530

    Adding stylings for the views

--- goals-app/App.js
+++ goals-app/App.js
@@ -1,34 +1,34 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Button } from "react-native";
+import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View>
-        <Text style={styles.textStyle}>Demo App</Text>
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your Goal" />
+        <Button title="Add Goal" />
       </View>
-      <Text
-        style={styles.textStyle}
-      >
-        Hello
-      </Text>
-      <Button title="tap me" />
+      <View>
+        <Text>list of gaols...</Text>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  appContainer: {
+    padding: 50,
   },
-  textStyle : { 
-    margin: 16,
-    borderWidth: 2,
-    borderColor: "green",
-    padding: 16
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#cccccc",
+    width: "80%",
+    marginRight: 8,
+    padding: 8,
   },
 });
